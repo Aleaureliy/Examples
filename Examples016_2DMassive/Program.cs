702d52485c2aff45b531c78ef486732b175d7507
Aleaureliy <easternevil@mail.ru>
2023-09-19 22:10:35 +0300

    лекция 4

---
+++ Examples016_2DMassive/Program.cs
@@ -0,0 +1,46 @@
+﻿// string[,] table = new string[2, 5];
+// table[1, 2] = "Слово";
+// for(int rows = 0; rows < 2; rows++)
+// {
+//     for(int colums = 0; colums < 5; colums++)
+//     {
+//         Console.WriteLine($"--{table[rows, colums]}--");
+//     }
+// }
+
+// for(int i = 0; i < 3; i++)
+// {
+//     for(int j = 0; j < 4; j++)
+//     {
+//         Console.Write($"{matrix[i, j]}");
+//     }
+//     Console.WriteLine();
+// }
+void PrintArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ Examples018_Recursia/Program.cs
@@ -0,0 +1,9 @@
+﻿double Factiorial (int number)
+{
+    if(number == 1) return 1;
+    else return number * Factiorial(number - 1);
+}
+for(int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factiorial(i)}");
+}
